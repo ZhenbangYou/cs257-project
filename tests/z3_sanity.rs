d80224d3c2e1340f215a9207ff3b407e328d736e
ZhenbangYou <zhenbangyou@gmail.com>
2023-12-07 13:54:26 -0800

    update z3_sanity

--- tests/z3_sanity.rs
+++ tests/z3_sanity.rs
@@ -1,6 +1,6 @@
 use z3::{
-    ast::{Array, Ast, Int},
-    Config, Context, FuncDecl, Solver, Sort,
+    ast::{self, Array, Ast, Int, Real},
+    Config, Context, FuncDecl, RecFuncDecl, SatResult, Solver, Sort,
 };
 
 #[test]
@@ -9,12 +9,11 @@ fn sanity_check() {
     let ctx = Context::new(&cfg);
     let solver = Solver::new(&ctx);
 
-    let sort_z = Sort::int(&ctx);
-    let f = FuncDecl::new(&ctx, "f", &[&sort_z], &sort_z);
+    let sort_int = Sort::int(&ctx);
+    let f = FuncDecl::new(&ctx, "f", &[&sort_int], &sort_int);
     let x = Int::new_const(&ctx, "x");
     let y = Int::new_const(&ctx, "y");
-    let _z = Int::new_const(&ctx, "z");
-    let a = Array::new_const(&ctx, "A", &sort_z, &sort_z);
+    let a = Array::new_const(&ctx, "A", &sort_int, &sort_int);
 
     // fml = Implies(x + 2 == y, f(Store(A, x, 3)[y - 2]) == f(y - x + 1))
     let left = (&x + Int::from_i64(&ctx, 2))._eq(&y);
@@ -27,6 +26,74 @@ fn sanity_check() {
 
     solver.assert(&fml.not());
 
-    println!("Solver: {:?}", solver.check());
+    assert_eq!(solver.check(), SatResult::Unsat);
+    println!("Model: {:?}", solver.get_model());
+}
+
+#[test]
+fn test_real_cmp() {
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let solver = Solver::new(&ctx);
+
+    let x = Real::new_const(&ctx, "x");
+    let sort_real = Sort::real(&ctx);
+    let f = FuncDecl::new(&ctx, "f", &[&sort_real], &sort_real);
+    let f_x = f.apply(&[&x]);
+    // forall x, f(x) = x + 1
+    let forall = ast::forall_const(
+        &ctx,
+        &[&x],
+        &[],
+        &(&x + &Real::from_real(&ctx, 1, 1))._eq(&f_x.as_real().unwrap()),
+    );
+
+    solver.assert(&forall);
+    assert_eq!(solver.check(), SatResult::Sat);
+    let model = solver.get_model().unwrap();
+    println!("Model: {:?}", model);
+
+    let f_100 = model
+        .eval(
+            &f.apply(&[&Real::from_int(&Int::from_i64(&ctx, 100))]),
+            true,
+        )
+        .unwrap()
+        .as_real()
+        .unwrap()
+        .as_real()
+        .unwrap();
+    println!("{:?}", f_100);
+}
+
+#[test]
+fn test_rec_func_def() {
+    let cfg = Config::new();
+
+    let ctx = Context::new(&cfg);
+
+    let fac = RecFuncDecl::new(&ctx, "fac", &[&Sort::int(&ctx)], &Sort::int(&ctx));
+    let n = Int::new_const(&ctx, "n");
+    let n_minus_1 = Int::sub(&ctx, &[&n, &ast::Int::from_i64(&ctx, 1)]);
+    let fac_of_n_minus_1 = fac.apply(&[&n_minus_1]);
+    let cond = n.le(&Int::from_i64(&ctx, 0));
+    let body = cond.ite(
+        &ast::Int::from_i64(&ctx, 1),
+        &ast::Int::mul(&ctx, &[&n, &fac_of_n_minus_1.as_int().unwrap()]),
+    );
+
+    fac.add_def(&[&n], &body);
+
+    let x = ast::Int::new_const(&ctx, "x");
+    let y = ast::Int::new_const(&ctx, "y");
+
+    let solver = Solver::new(&ctx);
+
+    solver.assert(&x._eq(&fac.apply(&[&ast::Int::from_i64(&ctx, 4)]).as_int().unwrap()));
+    solver.assert(&y._eq(&ast::Int::mul(&ctx, &[&ast::Int::from_i64(&ctx, 5), &x])));
+    solver.assert(&y._eq(&fac.apply(&[&ast::Int::from_i64(&ctx, 5)]).as_int().unwrap()));
+    solver.assert(&y._eq(&ast::Int::from_i64(&ctx, 120)));
+
+    assert_eq!(solver.check(), SatResult::Sat);
     println!("Model: {:?}", solver.get_model());
 }
